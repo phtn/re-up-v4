3d82d7c004e35d5d5a0ef3a3410955a1f4fad39f
Jun Orville Lecena <jun.lecena@comptrolla.com>
2024-01-17 09:49:24 +0800

    fix: bgcolor to black

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,16 @@ import { Jumbotron } from "./jumbotron";
 
 export default function Home() {
   return (
-    <main>
+    <main className="">
       <div className="absolute z-50 flex h-[25vh] w-screen items-end px-[5px] md:px-[250px]">
         <Brand />
       </div>
       <div className="z-40 h-[60vh]">
         <Jumbotron />
       </div>
-      <div className="absolute z-50 w-screen items-end justify-center md:h-[20vh] md:px-24">
+      <div className="absolute z-50 w-screen items-end justify-center bg-black md:h-[20vh] md:bg-transparent md:px-24">
         <Features />
-        <div className="flex h-44 items-center justify-center text-[12px] font-light text-neutral-500">
+        <div className="flex h-44 items-center justify-center bg-black text-[12px] font-light text-neutral-500">
           &copy; Re-up.ph. All rights reserved. {new Date().getFullYear()}
         </div>
       </div>
