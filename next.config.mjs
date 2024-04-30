c88155786dc4f1ef9b07a4788a647e9c4e473364
re-up-hq <hq@re-up.ph>
2024-04-30 22:41:09 +0800

    rm: PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,7 @@
-import withPWAInit from "@ducanh2912/next-pwa";
 await import("./src/env.js");
-
-const withPWA = withPWAInit({
-  dest: "public",
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-  disable: false,
-  workboxOptions: {
-    disableDevLogs: true,
-  },
-  register: true,
-});
-
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
 };
 
-export default withPWA(config);
+export default config;
